e19752524bd1df8dd2b4e6c10bbfdfc6652efc53
joshua richards <jermain119@gmail.com>
2019-10-19 21:05:43 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,7 +1,33 @@
 # code here!
-class School 
-school = School.new("Bayside High School")
-school.add_student("Zach Morris", 9)
-school.roster
+require 'pry'
 
+class School
+
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(name, grade)
+    @student_name = name
+    @grade = grade
+    if @roster.include?(grade) == false
+      @roster[grade] = []
+    end
+    @roster[grade] << name
+  end
+
+  def grade(number)
+    @roster[number]
+  end
+
+  def sort
+    @roster.each do |grade, name|
+      @roster[grade] = name.sort
+    end
+  end
 end
